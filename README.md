2de6cee0f27206c048ef3c23ac7aab472c64acd7
Denis Voloshin <d.voloshin@infosec.ru>
2017-10-12 19:00:42 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,22 +1,28 @@
-# Ближайшие бары
+# Nearest bars
 
-[FIXME. Здесь будет описание проекта]
+Script analyze info about bars from [data.mos.ru](https://data.mos.ru/) and prints out biggest, smallest and nearest ones
 
-# Как запустить
+#### Run script:
+```
+python bars.py <filename> <longitude> <latitude>
+```
+where  `<filename>` - json filename, `<longitude>` and ` <latitude>` current geo coordinates
 
-Скрипт требует для своей работы установленного интерпретатора Python версии 3.5
+#### Output:
+Three lines with biggest, smallest and nearest bars, describing their names, amount of seats and distance to each of them
 
-Запуск на Linux:
-
-```#!bash
-
-$ python bars.py # possibly requires call of python3 executive instead of just python
-# FIXME вывести пример ответа скрипта
+#### Run script example:
+```
+python bars.py bars.json 37.635709999610896, 55.805575000158512
+```
+#### Output example:
+```
+Biggest bar named Sport bar «Red Machine», has 450 seats and located in 11.609 km
+Smallest bar named BAR. Juices, has 0 seats and located in 17.908 km
+Closest bar named Bar «Jonnie Green Pub», has 50 seats and located in 0.000 km
 
 ```
 
-Запуск на Windows происходит аналогично.
+# Project Goals
 
-# Цели проекта
-
-Код создан в учебных целях. В рамках учебного курса по веб-разработке - [DEVMAN.org](https://devman.org)
+The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- bars.py
+++ bars.py
@@ -1,21 +1,89 @@
+import codecs
 import json
+from math import radians, sin, cos, asin, sqrt
+
+import sys
+
+
+class Bar:
+    def __init__(self, name, seats, coordinates):
+        self.name = name
+        self.seats = seats
+        self.coordinates = coordinates
+
+    def __str__(self):
+        return 'bar named {}, has {} seats and located in {} km'.format(
+            self.name, self.seats, "%.3f" % self.distance)
+
+    def get_distance(self, longitude, latitude):
+        # convert decimal degrees to radians
+        lon1, lat1, lon2, lat2 = map(radians,
+                                     [longitude, latitude, self.coordinates[0],
+                                      self.coordinates[1]])
+        delta_longitude = lon2 - lon1
+        delta_latitude = lat2 - lat1
+        # haversine formula
+        var_a = sin(delta_latitude / 2) ** 2 + cos(lat1) * cos(lat2) * sin(
+            delta_longitude / 2) ** 2
+        var_c = 2 * asin(sqrt(var_a))
+        earth_radius = 6367
+        return var_c * earth_radius
 
 
 def load_data(filepath):
-    pass
+    with codecs.open(filepath, 'r', encoding='utf-8',
+                     errors='ignore') as opened_file:
+        return json.loads(opened_file.read())
 
 
-def get_biggest_bar(data):
-    pass
+def parse_bars(json_data):
+    bars = []
+    for entry in json_data['features']:
+        bar = Bar(entry['properties']['Attributes']['Name'],
+                  entry['properties']['Attributes']['SeatsCount'],
+                  entry['geometry']['coordinates'])
+        bars.append(bar)
+    return bars
 
 
-def get_smallest_bar(data):
-    pass
+def get_biggest_bar(bars):
+    biggest = None
+    max_seats = 0
+    for bar in bars:
+        if bar.seats > max_seats:
+            biggest = bar
+            max_seats = bar.seats
+    return biggest
 
 
-def get_closest_bar(data, longitude, latitude):
-    pass
+def get_smallest_bar(bars):
+    smallest = None
+    min_seats = 10000
+    for bar in bars:
+        if bar.seats < min_seats:
+            smallest = bar
+            min_seats = bar.seats
+    return smallest
+
+
+def get_closest_bar(bars, longitude, latitude):
+    closest_bar = None
+    min_distance = 20000
+    for bar in bars:
+        distance = bar.get_distance(longitude, latitude)
+        bar.distance = distance
+        if distance < min_distance:
+            closest_bar = bar
+            min_distance = distance
+    return closest_bar
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data(sys.argv[1])
+    bars = parse_bars(data)
+    closest_bar = get_closest_bar(bars, int(sys.argv[2]), int(sys.argv[3]))
+    smallest_bar = get_smallest_bar(bars)
+    biggest_bar = get_biggest_bar(bars)
+    print("Biggest {}".format(biggest_bar))
+    print("Smallest {}".format(smallest_bar))
+    print("Nearest {}".format(closest_bar))
